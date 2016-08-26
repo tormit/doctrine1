36920a1f7f1e8ab6eb5d8b36f44d501a5c418126
Tormi Talv <tormit@gmail.com>
2016-08-26 15:06:50 +0300

    Doctrine data import debug info output

--- lib/Doctrine/Data/Import.php
+++ lib/Doctrine/Data/Import.php
@@ -110,11 +110,15 @@ class Doctrine_Data_Import extends Doctrine_Data
     public function doImport($append = false)
     {
         $array = $this->doParsing();
+        printf("Finished parsing files.\n");
 
         if ( ! $append) {
+            printf("Purging data...");
             $this->purge(array_reverse(array_keys($array)));
+            printf("done.\n");
         }
 
+        printf("Processing and saving data...\n");
         $this->_loadData($array);
     }
 
@@ -343,6 +347,12 @@ class Doctrine_Data_Import extends Doctrine_Data
                 foreach ($this->_importedObjects as $obj) {
 
                     if ($obj instanceof $model) {
+                        if (method_exists($obj, '__toString')) {
+                            printf("Saving model: %s (%s)\n", $model, $obj);
+                        } else {
+                            printf("Saving model: %s\n", $model);
+                        }
+
                         $obj->save();
                     }
                 }
